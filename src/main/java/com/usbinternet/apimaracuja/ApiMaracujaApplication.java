d24f9b1b6d55d1c06839afb45d27f3bdf34f7c4f
Rennan Silva Costa <rennansilvacosta@hotmail.com>
2020-05-08 11:55:34 -0300

    estrutura base do projeto

---
+++ src/main/java/com/usbinternet/apimaracuja/ApiMaracujaApplication.java
@@ -0,0 +1,13 @@
+package com.usbinternet.apimaracuja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMaracujaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMaracujaApplication.class, args);
+	}
+
+}
