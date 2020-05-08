1086e895ba2e8f3f0e3bf84291735f6fa0d9c554
Rennan Silva Costa <rennansilvacosta@hotmail.com>
2020-05-08 12:40:17 -0300

    testando classe de dominio

--- src/main/java/com/usbinternet/apimaracuja/resources/EnderecoResource.java
+++ src/main/java/com/usbinternet/apimaracuja/resources/EnderecoResource.java
@@ -1,16 +1,30 @@
 package com.usbinternet.apimaracuja.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.usbinternet.apimaracuja.domain.Endereco;
 
 @RestController
 @RequestMapping(value = "/enderecos")
 public class EnderecoResource {
 
-	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Rest está funcionando";
+	@RequestMapping(method = RequestMethod.GET)
+	public List<Endereco> listar() {
+
+		Endereco e1 = new Endereco(1, "04929080", "Avenida Professor Mário Mazagão", "Alto da Riviera");
+		Endereco e2 = new Endereco(2, "04929290", "Ricardina Viera Coelho", "Alto da Riviera");
+
+		List<Endereco> lista = new ArrayList<>();
+
+		lista.add(e1);
+		lista.add(e2);
+
+		return lista;
 	}
 
 }
