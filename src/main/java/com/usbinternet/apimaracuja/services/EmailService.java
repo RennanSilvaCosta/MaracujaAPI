dd5afbb90c906a567d68be239d115308c2fefbfd
Rennan Silva Costa <rennansilvacosta@hotmail.com>
2020-08-12 20:40:07 -0300

    implementando envio de email html

--- src/main/java/com/usbinternet/apimaracuja/services/EmailService.java
+++ src/main/java/com/usbinternet/apimaracuja/services/EmailService.java
@@ -1,5 +1,7 @@
 package com.usbinternet.apimaracuja.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.usbinternet.apimaracuja.domain.Usuario;
@@ -8,6 +10,10 @@ public interface EmailService {
 
 	void sendRegistrationConfirmationEmail(Usuario user);
 	
+	void sendRegistrationConfirmationHtmlEmail(Usuario user);
+	
 	void sendEmail(SimpleMailMessage msg);
+	
+	void sendHtmlEmail(MimeMessage msg);
 	
 }
