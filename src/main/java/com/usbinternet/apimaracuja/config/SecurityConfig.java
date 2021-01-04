39f6b707e5c819049374f235760b1a0b2286bf90
Rennan Silva Costa <rennansilvacosta@hotmail.com>
2021-01-04 14:31:08 -0300

    send email configs

--- src/main/java/com/usbinternet/apimaracuja/config/SecurityConfig.java
+++ src/main/java/com/usbinternet/apimaracuja/config/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	private static final String[] PUBLIC_MATCHERS_POST = {
 			"/usuarios",
-			"/auth/forgot/**"
+			"/auth/forgot"
 	};
 
 	@Override
